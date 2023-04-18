4e3344cb19a47856805de97dfa8e9130d63172c3
NastyaKatyushkina <nastyusha_kat@mail.ru>
2023-04-18 15:13:15 +0500

    use-json-fetch-f

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import SuccessfulData from './components/SuccessfulData';
+import ErrorData from './components/ErrorData';
+import LoadingData from './components/LoadingData';
+
+function App() {
+    return (
+        <>
+            <SuccessfulData />
+            <ErrorData />
+            <LoadingData />
+        </>
+    );
+}
+
+export default App;
